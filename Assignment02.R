63fd294933c3271dd008001e8f34e16d99286dbe
fgao38 <fgao38@uchicago.edu>
2024-01-07 13:35:31 -0600

    Assignment 2 @nrdowling @ysh627

--- Assignment02.R
+++ Assignment02.R
@@ -6,11 +6,14 @@ library(tinytex)
 
 #Numeric value
 Number = 5
+print(Number)
 
 #String value
 String = "Hi"
+print(String)
 
 #Creating a dataframe
 my_dict <- list(tom = 1, david = 2, sam = 3)
 my_df <- data.frame(names = names(my_dict), values = unlist(my_dict))
+print(my_df)
 
